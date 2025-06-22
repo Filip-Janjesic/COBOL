85d8db21d6fff6257c3654a3d2dc5a06fc9347de
Filip Janješić <filip.janjesic@gmail.com>
2025-06-22 20:16:10 +0200

    Remove old COBOL program files and markdown documentation

--- hello_world/hello_world.cob
+++
@@ -1,7 +0,0 @@
-       IDENTIFICATION DIVISION.
-              PROGRAM-ID. HELLO_WORLD.
-              PROCEDURE DIVISION.
-                  DISPLAY "Hello, World!".
-                  STOP RUN.
-                END PROGRAM HELLO_WORLD.
-       
