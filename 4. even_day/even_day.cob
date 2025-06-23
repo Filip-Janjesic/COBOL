1fd68f5581861841bc95177de8f44f96fd9e5846
Filip Janješić <filip.janjesic@gmail.com>
2025-06-23 22:01:31 +0200

    Update even_day COBOL program

--- 4. even_day/even_day.cob
+++ 4. even_day/even_day.cob
@@ -1,1 +1,24 @@
-test
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. EVEN_DAY.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+           01 WS-DAY-NAME    PIC X(10).
+           01 WS-DAY-NUMBER  PIC 99.
+
+       PROCEDURE DIVISION.
+           DISPLAY "Enter the day name: ".
+           ACCEPT WS-DAY-NAME.
+
+           DISPLAY "Enter the day number (1-31): ".
+           ACCEPT WS-DAY-NUMBER.
+
+           IF FUNCTION MOD(WS-DAY-NUMBER, 2) = 0
+               DISPLAY "The day is even."
+           ELSE
+               DISPLAY "The day is odd."
+           END-IF.
+
+           STOP RUN.
+       END PROGRAM EVEN_DAY.
+       
